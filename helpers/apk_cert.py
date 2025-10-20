372114ff2113e91cc8282b527471a422f35ab136
d4Rk <darkdevilx@gmx.net>
2025-10-20 12:39:12 +0200

    fixed format of sha256 digest

--- helpers/apk_cert.py
+++ helpers/apk_cert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import textwrap
 
 KEYTOOL_PATH = 'apksigner'
 
@@ -8,10 +9,10 @@ def get_sha256_cert_fingerprint(apk):
     apk_cert = subprocess.Popen(
         KEYTOOL_PATH + ' verify --print-certs ' + apk, shell=True, stdout=subprocess.PIPE
     ).stdout.read().decode()
-    print(apk_cert)
     if 'SHA-256 digest: ' in apk_cert:
         components = apk_cert.split('1 certificate SHA-256 digest: ')
         print(components)
         if len(components) > 1:
-            return components[1].split('\n')[0]
+            sha256_digest = components[1].split('\n')[0]
+            return ':'.join(textwrap.wrap(sha256_digest.upper(), 2))
     return None 
